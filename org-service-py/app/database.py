2b6add5a011954891b416e006bdc4b7731e2ec7f
MarquezAlan1 <marquezalanivan028@gmail.com>
2025-11-10 20:44:04 -0400

    Delete org-service-py directory

--- org-service-py/app/database.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict
-from .models import Unit
-
-store: Dict[str, Unit] = {}
-
-# Seed
-root = Unit(id="root-001", name="Company", parentId=None)
-store[root.id] = root
--- org-service-py/app/schemas.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional, List
-from pydantic import BaseModel
-
-class UnitResponse(BaseModel):
-    id: str
-    name: str
-    parentId: Optional[str]
-    managerId: Optional[str]
